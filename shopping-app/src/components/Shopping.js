b3acdd1adf59455ca665caeddf1593ab0394caec
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-02-08 01:22:52 +0530

    create sep component for stockData and use it in shopping component

--- shopping-app/src/components/Shopping.js
+++ shopping-app/src/components/Shopping.js
@@ -1,54 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Table from "./img/tab.jpg";
 import List from "./img/list.png";
-import Samsung1 from "./img/s20.jpg";
-import Vivo1 from "./img/v23.jpg";
-import Google1 from "./img/google-pixel-7pro.jpg";
-import Oppo1 from "./img/appo-reno8.jpg";
-import Apple1 from "./img/iphone13.jpg";
-import Apple2 from "./img/iphone14.jpg";
+
+import {stockData} from './stockData'
 
 export const Shopping = () => {
-  //data of products
-  const stockData = [
-    {
-      id: 1,
-      name: "OPPO RENO 8 5G",
-      price: 23400,
-      image: Oppo1,
-    },
-    {
-      id: 2,
-      name: "SUMSUNG S20FE",
-      price: 35000,
-      image: Samsung1,
-    },
-    {
-      id: 3,
-      name: "VIVO V23 PRO",
-      price: 45000,
-      image: Vivo1,
-    },
-    {
-      id: 4,
-      name: "GOOGLE PIXEL 7",
-      price: 55000,
-      image: Google1,
-    },
-    {
-      id: 5,
-      name: "APPLE IPHONE 14",
-      price: 79000,
-      image: Apple1,
-    },
-    {
-      id: 6,
-      name: "APPLE IPHONE 13",
-      price: 59000,
-      image: Apple2,
-    },
-  ];
-
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [format, setFormat] = useState("list");
---
+++ shopping-app/src/components/stockData.js
@@ -0,0 +1,50 @@
+import Samsung1 from "./img/s20.jpg";
+import Vivo1 from "./img/v23.jpg";
+import Google1 from "./img/google-pixel-7pro.jpg";
+import Oppo1 from "./img/appo-reno8.jpg";
+import Apple1 from "./img/iphone13.jpg";
+import Apple2 from "./img/iphone14.jpg";
+export const stockData = [
+    {
+      id: 1,
+      name: "OPPO RENO 8 5G",
+      price: 23400,
+      image: Oppo1,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: "SUMSUNG S20FE",
+      price: 35000,
+      image: Samsung1,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "VIVO V23 PRO",
+      price: 45000,
+      image: Vivo1,
+      quantity: 1,
+    },
+    {
+      id: 4,
+      name: "GOOGLE PIXEL 7",
+      price: 55000,
+      image: Google1,
+      quantity: 1,
+    },
+    {
+      id: 5,
+      name: "APPLE IPHONE 14",
+      price: 79000,
+      image: Apple1,
+      quantity: 1,
+    },
+    {
+      id: 6,
+      name: "APPLE IPHONE 13",
+      price: 59000,
+      image: Apple2,
+      quantity: 1,
+    },
+  ];

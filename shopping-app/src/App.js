bb6c2542342e6a9b42db2c9939abd01ce2338ddf
lokeshjawale1996 <jawalelokesh2411@gmail.com>
2023-02-07 16:19:00 +0530

    shopping project created with tailwind css setup

---
+++ shopping-app/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline bg-slate-300">
+      Welcome To Shopping App!
+    </h1>
+
+    </div>
+  );
+}
+
+export default App;
